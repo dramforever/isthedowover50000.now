11e24d3a02f9df514d2e82e1c4ca57ead0c6e3a9
dramforever <dramforever@live.com>
2026-02-18 02:53:56 +0800

    Make it fail when it fails

--- update.sh
+++ update.sh
@@ -3,8 +3,8 @@ domain="isthedowover50000.now"
 server="https://grebedoc.dev"
 user_agent="See GitHub dramforever/isthedowover50000.now"
 
-set -x
-curl --user-agent "$user_agent" -H "X-Api-Key: ${API_NINJAS_TOKEN}" 'https://api.api-ninjas.com/v1/stockprice?ticker=%5EDJI' > data.json
+set -euxo pipefail
+curl --fail-with-body --user-agent "$user_agent" -H "X-Api-Key: ${API_NINJAS_TOKEN}" 'https://api.api-ninjas.com/v1/stockprice?ticker=%5EDJI' > data.json
 cat data.json >&2
 jq -r -f proc.jq < data.json > site/index.html
 cat site/index.html >&2
